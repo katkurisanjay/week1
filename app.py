24747f111de0300c83c8507d68b2446a1195a2ab
Nithish Arram <nithish.niroggyan@gmail.com>
2025-02-09 19:26:21 +0530

    added huggingface

--- app.py
+++ app.py
@@ -3,7 +3,32 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+import cv2
+from huggingface_hub import hf_hub_download
+import tensorflow as tf
+# Define the repository ID and filename
+repo_id = "sanjaykatkuri/wasteclassification"
+filename = "my_waste_classification_model.keras"
+# Download the model file
+model_path = hf_hub_download(repo_id=repo_id, filename=filename)
+# Load the model
+model = tf.keras.models.load_model(model_path)
 
+def predict_fun(img):
+    plt.figure(figsize=(6,4))
+    # Change 'cv2.Color' to 'cv2.cvtColor'
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.tight_layout()
+    img = cv2.resize(img,(224,224))
+    img = np.reshape(img, [-1, 224, 224,3])
+    result = np.argmax(model.predict(img))
+    if result == 0:
+        print("the image is Recyclable waste")
+        return "The image is Recyclable waste"
+    elif result == 1:
+        print("the image show is Organic waste")
+        return "The image show is Organic waste"
 
 def main():  
     # Set the title and layout  
@@ -25,7 +50,8 @@ def main():
     if st.button("Classify Image"):  
         if uploaded_file is not None:  
             # Code to process the uploaded image  
-            st.success("Image uploaded successfully!")  
+            result = predict_fun(uploaded_file)
+            st.success(result)  
             # You can add your model prediction code here  
         elif image_url:  
             # Code to process the image URL  
