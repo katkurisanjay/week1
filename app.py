0d2bcbf1dfb3f1de68e7405973ced4c2a532e19c
Nithish Arram <nithish.niroggyan@gmail.com>
2025-02-09 17:46:17 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st  
+from wasteclassification import load_images  # Import your functions from the Jupyter notebook  
+
+def main():  
+    st.title("My Jupyter Notebook App")  
+    
+    # Add your content here, for example:  
+    st.write("Welcome to my Streamlit app!")  
+    
+    # Call your main function from the notebook  
+    result = load_images()  
+    
+    # Display the result  
+    st.write(result)  
+    
+if __name__ == "__main__":  
+    main()
