0e31cf5a6afbfb25b729e372761287efb908549e
Nithish Arram <nithish.niroggyan@gmail.com>
2025-02-09 17:54:22 +0530

    added app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st  
 # from wasteclassification import load_images  # Import your functions from the Jupyter notebook  
+import pandas as pd
+import numpy as np
+import time
+
 
 def main():  
     st.title("My Jupyter Notebook App")  
@@ -12,6 +16,15 @@ def main():
     
     # Display the result  
     st.write(result)  
+    df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
+    my_data_element = st.line_chart(df)
+
+    for tick in range(10):
+        time.sleep(.5)
+        add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
+        my_data_element.add_rows(add_df)
+
+    st.button("Regenerate")
     
 if __name__ == "__main__":  
     main()
